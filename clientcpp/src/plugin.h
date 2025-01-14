6c4b8394b81e9f9d50165ab5f10685030b93f525
Eduardo Galvani Massino <eduardo.floyd@outlook.com>
2025-01-14 07:28:44 -0300

    Updated for MacOS

--- clientcpp/src/plugin.h
+++ clientcpp/src/plugin.h
@@ -1,13 +1,13 @@
 /** \file plugin.h
  * \brief Contém macros para acesso à bibliotecas dinâmicas que usam o padrão ANSI-C, como bibliotecas Fortran.
  */
-
 #ifndef __PLUGIN_H__
 #define __PLUGIN_H__
 
 #if defined(_WIN32) || defined(_WIN64)
 #include <windows.h>
 #define LIB_HANDLE HMODULE
+#define FUNC_PTR FARPROC
 #define LOAD_LIBRARY(name) LoadLibrary(name)
 #define GET_PROC_ADDRESS GetProcAddress
 #define FREE_LIBRARY FreeLibrary
@@ -16,6 +16,7 @@
 #include <dlfcn.h>
 #include <unistd.h>
 #define LIB_HANDLE void*
+#define FUNC_PTR void*
 #define LOAD_LIBRARY(name) dlopen(name, RTLD_LOCAL | RTLD_LAZY)
 #define GET_PROC_ADDRESS dlsym
 #define FREE_LIBRARY dlclose
@@ -23,8 +24,7 @@
 #endif
 
 LIB_HANDLE load_library(const char* libFile) {
-  LIB_HANDLE libHandle;
-  libHandle = LOAD_LIBRARY(libFile);
+  LIB_HANDLE libHandle = LOAD_LIBRARY(libFile);
   if (!libHandle) {
     std::cerr << "Erro ao carregar a biblioteca " << libFile << std::endl;
     exit(1);
@@ -32,8 +32,8 @@ LIB_HANDLE load_library(const char* libFile) {
   return libHandle;
 }
 
-auto load_function(LIB_HANDLE libHandle, const char* function) {
-  auto func = GET_PROC_ADDRESS(libHandle, function);
+FUNC_PTR load_function(LIB_HANDLE libHandle, const char* function) {
+  FUNC_PTR func = GET_PROC_ADDRESS(libHandle, function);
   if (!func) {
     std::cerr << "Erro ao localizar o simbolo " << function <<  " na lib" << std::endl;
     FREE_LIBRARY(libHandle);
